775835f1904320910b533b192d1d850f2a46b63c
Ron Henson <ron.@debiron.com>
2022-09-08 09:07:48 -0500

    submission Bite 337 @ codechalleng.es

--- fastapi/0337/models.py
+++ fastapi/0337/models.py
@@ -1,3 +1,11 @@
 from pydantic import BaseModel
 
 # write a Food pydantic model
+class Food(BaseModel):
+    """ Using pydantic to define a Food model"""
+    id : int
+    name : str
+    serving_size : str
+    kcal_per_serving : int
+    protein_grams : float
+    fibre_grams : float = 0
