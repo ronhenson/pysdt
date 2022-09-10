339dcc49def2610c022cafbd1393d4300d28b4d4
Ron Henson <ron.@debiron.com>
2022-09-10 05:32:47 -0500

    submission Bite 341 @ codechalleng.es

--- fastapi/0341/models.py
+++ fastapi/0341/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+import pwd
 from typing import Any
 
 from passlib.context import CryptContext
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 # https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/
 # which we'll further explore in a later Bite
@@ -25,3 +26,29 @@ class Food(BaseModel):
 
 
 # Write the User and FoodEntry models here ...
+
+class User(BaseModel):
+    """User attributes Bite 341"""
+
+    id: int
+    username: str
+    password: str
+
+    @validator("password")
+    def hash_password(cls, pw):
+        return get_password_hash(pw)
+
+
+class FoodEntry(BaseModel):
+    """Food entry atrributes Bite 341"""
+
+    id: int
+    user: User
+    food: Food
+    date_added: datetime = datetime.now()
+    number_servings: float
+
+    @property
+    def total_calories(self):
+        """Calculate total kilocalories"""
+        return self.number_servings * self.food.kcal_per_serving
